475ee6ce19a9e07cb5e972f47891b3429d189277
Gene Campbell <gcampbell391@gmail.com>
2021-12-28 16:08:09 -0500

    Home screen with Header and Menu component created

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Header = () => {
+
+    return(
+        <div id="header-flex-box">
+            <div id="header-flex-box-left">
+                <h1>Woodstock Bar & Grill</h1>
+            </div>
+            <div id="header-flex-box-right">
+                <h1>NavBar</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/screens/Home.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Header from "../components/Header";
+import Menu from "../components/Menu";
+
+const Home = () => {
+
+    return(
+        <div>
+            <Header/>
+            <h1>Menu Title</h1>
+            <Menu/>
+        </div>
+    )
+}
+
+
+export default Home;
